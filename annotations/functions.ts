5b563997bcf84dfd28578455b310723e62b4d86f
David Christian Jones <david@dcj.codes>
2019-07-15 15:46:24 -0400

    Destructuring additions

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -3,6 +3,9 @@
 // - the types of function arguments
 // - the type of return value (if any)
 
+// Function Expressions/Anonymous Functions
+// ----------------------------------------
+
 // Annotate return value
 const generateName = (a: string, b: string): string => {
   return `${a} ${b}`;
@@ -13,3 +16,26 @@ console.log(generateName("David", "Jones"));
 const logName = (a: string, b: string): void => {
   console.log(`${a} ${b}`);
 };
+
+const multiply = function(a: number, b: number): number {
+  return a * b;
+};
+
+// Function Declarations
+// --------------------
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+function logIt(message: string): void {
+  console.log(message);
+}
+
+// Destructuring objects in Functions
+const data = { name: "David", age: 32 };
+
+const logData = ({ name, age }: { name: string; age: number }): void => {
+  console.log(name, age);
+};
+
+logData(data);
