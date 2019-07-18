459fa88c22b13a35e02a60bbb53a6e456d9c0ed7
David Jones <david@dcj.codes>
2019-07-18 13:27:16 -0400

    Tuples

---
+++ tuple/_overview.ts
@@ -0,0 +1,13 @@
+// A tuple is very similar to an array in structure.
+// The key difference from an array is that each member
+// represents one particular property of a single record,
+// whereas arrays are usually a collection of records
+
+// Object format
+const chap = { name: "David", age: 32, weight: 163 };
+
+// Tuple format
+const tuChap = ["David", 32, 163];
+// Each element of the array/tuple relates to a single thing,
+// in this case a person, and the ordering
+// would need to be fixed when working we multiple instances.
