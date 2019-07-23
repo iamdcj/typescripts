1ed499d5c2eb693024346f14d0ae5e17b13fea43
David Jones <david@dcj.codes>
2019-07-23 13:12:33 -0400

    TS Training : Restructure

--- _types/never.ts
+++ _types/never.ts
@@ -1,5 +1,6 @@
 // The NEVER type is used if a function does not return anything at all,
 // it doesn't return value, nor null or undefined, it simply never completes
+// e.g. when a function throws an error
 
 // EXAMPLE 1
 function ThrowError(message: string): never {
