f9171e3aaf14f917c634301181d26b3dc0a27929
David Jones <david@dcj.codes>
2019-07-24 12:53:26 -0400

    TS Training : Decorators start

---
+++ decorators/README.md
@@ -0,0 +1,13 @@
+# TypeScript Decorators
+
+### What are Decorators in TS?
+
+They are functions that can be used to modify the properties and methods in a Class.
+
+They aren't the same as Decorators in regular JavaScript.
+
+They can be used inside or on a class.
+
+The execution order is the key to understanding and appreciating them.
+
+They are totally experimental.
---
+++ decorators/decorators.ts
@@ -0,0 +1,22 @@
+class Boat {
+  // Property instance
+  color: string = "orange";
+
+  // Accessor: getter
+  get formattedColor(): string {
+    return `The color of this boat is ${this.color}`;
+  }
+
+  // Method
+
+  @testDecorator
+  pilot(): void {
+    console.log("swish");
+  }
+}
+
+// Decorator has a target argument, and a key argument
+function testDecorator(target: any, key: string): void {
+  console.log(target);
+  console.log(key);
+}
